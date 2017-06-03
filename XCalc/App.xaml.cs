a19f7e88c5849623ae6b32e7b6d3167d62131eae
Pavel Yosifovich <zodiacon@live.com>
2017-06-03 16:22:36 +0300

    made a single executable

--- XCalc/App.xaml.cs
+++ XCalc/App.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Windows;
 using XCalc.ViewModels;
@@ -12,6 +14,36 @@ namespace XCalc {
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+        readonly Dictionary<string, Assembly> _assemblies = new Dictionary<string, Assembly>(8);
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private void LoadAssemblies() {
+            var appAssembly = typeof(App).Assembly;
+            foreach (var resourceName in appAssembly.GetManifestResourceNames()) {
+                if (resourceName.EndsWith(".dll", StringComparison.InvariantCultureIgnoreCase)) {
+                    using (var stream = appAssembly.GetManifestResourceStream(resourceName)) {
+                        var assemblyData = new byte[(int)stream.Length];
+                        stream.Read(assemblyData, 0, assemblyData.Length);
+                        var assembly = Assembly.Load(assemblyData);
+                        _assemblies.Add(assembly.GetName().Name, assembly);
+                    }
+                }
+            }
+            AppDomain.CurrentDomain.AssemblyResolve += OnAssemblyResolve;
+        }
+
+        Assembly OnAssemblyResolve(object sender, ResolveEventArgs args) {
+            var shortName = new AssemblyName(args.Name).Name;
+            if (_assemblies.TryGetValue(shortName, out var assembly)) {
+                return assembly;
+            }
+            return null;
+        }
+
+        public App() {
+            LoadAssemblies();
+        }
+
         protected override void OnStartup(StartupEventArgs e) {
             base.OnStartup(e);
 
